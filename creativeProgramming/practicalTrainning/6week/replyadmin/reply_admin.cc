64104ada58686c3945b585a10f1085782e81498c
yudonlee <future0159@naver.com>
2019-11-15 23:02:34 +0900

    final check on 6week assignment

--- creativeProgramming/practicalTrainning/6week/replyadmin/reply_admin.cc
+++ creativeProgramming/practicalTrainning/6week/replyadmin/reply_admin.cc
@@ -2,20 +2,6 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-/*ReplyAdmin::ReplyAdmin() {
-    chats = new string[200];
-}
-ReplyAdmin::~ReplyAdmin() {
-    delete chats;
-}
-int ReplyAdmin::getChatCount() {
-    int i;
-    for (i = 0; i < NUM_OF_CHAT; ++i) {
-        if (chats[i].empty() == true)
-            break;
-    }
-    return i;
-}*/
 ReplyAdmin::ReplyAdmin() {
 }
 ReplyAdmin::~ReplyAdmin() {
@@ -31,7 +17,6 @@ bool ReplyAdmin::addChat(string _chats) {
     return true;
 }
 bool ReplyAdmin::removeChat(int _index) {
-    //int count = getChatCount();
     int size = chats.size();
     if ( size < _index)
         return false;
@@ -39,23 +24,6 @@ bool ReplyAdmin::removeChat(int _index) {
         list<string>::iterator itr = chats.begin();
         advance(itr, _index);
         itr = chats.erase(itr);
-        /*if (_index == 0) {
-            if (count == 1)
-                chats[0].clear();
-            else {
-                for (int i = 0; i < count - 1; i++)
-                    chats[i] = chats[i + 1];
-                chats[count].clear();
-            }
-        }
-        else {
-            if (count == _index - 1)
-                chats[_index].clear();
-            for (int i = _index; i < count; i++)
-                chats[i] = chats[i + 1];
-            chats[count].clear();
-
-        }*/
         return true;
     }
 }
@@ -70,7 +38,6 @@ bool ReplyAdmin::removeChat(int* _indices, int _count) {
         for (int i = 0; i < _count; i++) {
             if (indices[i] - trials > _count == true) 
                 return false;
-            // {} removed!
             else {
                 removeChat(indices[i] - trials);
                 trials++;
@@ -82,7 +49,6 @@ bool ReplyAdmin::removeChat(int* _indices, int _count) {
     return true;
 }
 bool ReplyAdmin::removeChat(int _start, int _end) {
-    //int count = getChatCount();
     int size = chats.size();
     if ( size <= _end)
         return false;
@@ -93,19 +59,4 @@ bool ReplyAdmin::removeChat(int _start, int _end) {
     advance(end, _end);
     chats.erase(start, end);
     return true;
-
-    /*advance(start, _start - 1);
-    
-    if (count < _end - _start + 1)
-        return false;
-    else
-        for (int i = start; i <= end; i++) {
-            if (chats[i].empty() == true)
-                return false;
-            else {
-                removeChat(i - trials);
-                trials++;
-            }
-
-        } */
 }
